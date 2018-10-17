ed04a934369ce434ec4427c21420133741cfc115
Mostafa AbdEl Fatah <mostafaabdelfatah@Mostafas-Mac.local>
2018-10-16 21:19:46 -0700

    first commit

--- lifecycle/ViewController.swift
+++ lifecycle/ViewController.swift
@@ -10,16 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    override func loadView() {
+        print("ViewController:loadView")
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        print("ViewController:viewDidLoad")
+        
     }
-
+    
+    override func viewWillLayoutSubviews() {
+        print("ViewController:viewWillLayoutSubviews")
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        print("ViewController:viewWillAppear")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        print("ViewController:viewDidAppear")
+    }
+    override func viewDidLayoutSubviews() {
+        print("ViewController:viewDidLayoutSubviews")
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        print("ViewController:viewWillDisappear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        print("ViewController:viewDidDisappear")
+    }
+    
 }
 
